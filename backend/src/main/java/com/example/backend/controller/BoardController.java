cf7533988649e4a15bb9718c3f8689fc76262d7d
Sophie-Ahn <sgdream95@gmail.com>
2024-04-30 17:19:24 +0900

    경비게시판 service test 및 어노테이션 추가

--- backend/src/main/java/com/example/backend/controller/BoardController.java
+++ backend/src/main/java/com/example/backend/controller/BoardController.java
@@ -1,9 +1,29 @@
 package com.example.backend.controller;
 
+import com.example.backend.dto.template.AccountDto;
+import com.example.backend.service.BoardService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Log4j2
 @RestController
 @RequestMapping("/board")
 public class BoardController {
+
+    @GetMapping("/accountRegister")
+    public void accountRegisterGet(){
+
+    }
+
+//    @PostMapping("/accountRegister")
+//    public String accountRegisterPost(AccountDto accountDto, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+//        if(bindingResult.hasErrors()){
+//
+//        }
+//    }
 }
--- backend/src/main/java/com/example/backend/repository/DocumentRepository.java
+++ backend/src/main/java/com/example/backend/repository/DocumentRepository.java
@@ -2,6 +2,8 @@ package com.example.backend.repository;
 
 import com.example.backend.entity.maria.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
 }
