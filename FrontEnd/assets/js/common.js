613323895536e74dff5470022ff7e5f6d097b06a
KimYung1994 <157556953+KimYung1994@users.noreply.github.com>
2024-05-08 17:24:52 +0900

    경로 수정 완료

--- FrontEnd/assets/js/common.js
+++ FrontEnd/assets/js/common.js
@@ -1,6 +1,6 @@
 function includeHeader() {
 	const xhr = new XMLHttpRequest();
-	xhr.open('GET', '../common/header.html', true);
+	xhr.open('GET', 'http://localhost:3200/common/header', true);
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState == 4 && xhr.status == 200) {
 			document.querySelector('header').innerHTML = xhr.responseText;
@@ -11,7 +11,7 @@ function includeHeader() {
 
 function includeFooter() {
 	const xhr = new XMLHttpRequest();
-	xhr.open('GET', '../common/footer.html', true);
+	xhr.open('GET', 'http://localhost:3200/common/footer', true);
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState == 4 && xhr.status == 200) {
 			document.querySelector('footer').innerHTML = xhr.responseText;
