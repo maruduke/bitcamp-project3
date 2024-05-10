d341900537e72ee8e03e8eda99e3e91db7e43195
maruduke <abab1117@naver.com>
2024-05-10 19:05:29 +0900

    board Controller getMapping

--- backend/src/main/java/com/example/backend/service/BoardService.java
+++ backend/src/main/java/com/example/backend/service/BoardService.java
@@ -4,9 +4,12 @@ import com.example.backend.dto.board.MyListDto;
 import com.example.backend.dto.board.WaitDto;
 import com.example.backend.entity.maria.User;
 import com.example.backend.entity.maria.enumData.DocState;
+import com.example.backend.entity.mongo.Template;
+import com.example.backend.entity.mongo.TypeData;
 import com.example.backend.repository.DocumentRepository;
 import com.example.backend.repository.RefRepository;
 import com.example.backend.repository.TaskProgressRepository;
+import com.example.backend.repository.TemplateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -20,6 +23,7 @@ import java.util.List;
 public class BoardService {
 
     private final DocumentRepository documentRepository;
+    private final TemplateRepository templateRepository;
 
     public Slice<MyListDto> getMyList(User user, Pageable pageable){
 
@@ -36,5 +40,13 @@ public class BoardService {
         List<DocState> stateList = Arrays.asList(DocState.PROCESS_1, DocState.PROCESS_2, DocState.PROCESS_3);
 
         return documentRepository.findByStateIn(user, pageable, stateList);
+    }
+
+    public Template<? extends TypeData> getTemplate(String documentId) {
+
+        Template<? extends TypeData> template = templateRepository.findById(documentId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 문서입니다."));
+
+        return template;
+
     }
 }
