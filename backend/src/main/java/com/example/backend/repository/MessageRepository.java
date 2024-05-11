9819673c9155b874265f318136df253d9dd452e1
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-05-11 20:52:41 +0900

    페이지네이션 백 완성

--- backend/src/main/java/com/example/backend/repository/MessageRepository.java
+++ backend/src/main/java/com/example/backend/repository/MessageRepository.java
@@ -2,15 +2,26 @@ package com.example.backend.repository;
 
 import com.example.backend.entity.maria.Message;
 import com.example.backend.entity.maria.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    List<Message> findByReceiverId(User receiverId);
-    List<Message> findBySenderId(User senderId);
+    @Query("SELECT m FROM Message m WHERE m.receiverId = :receiverId AND m.readCheck = false AND m.receiverDelete = false")
+    Page<Message> findByNoReadReceiverId(User receiverId, Pageable pageable);
+
+    @Query("SELECT m FROM Message m WHERE m.receiverId = :receiverId AND m.receiverDelete = false")
+    Page<Message> findByReceiverId(User receiverId, Pageable pageable);
+
+    @Query("SELECT m FROM Message m WHERE m.senderId = :senderId AND m.senderDelete = false")
+    Page<Message> findBySenderId(User senderId, Pageable pageable);
+
     void deleteByMessageId(Long messageId);
 
 
