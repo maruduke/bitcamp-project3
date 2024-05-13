af271f874db228414dbb3351778461c1311d5246
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-05-13 12:51:13 +0900

    메인 나의 부서 연락쳐 멤버 완성

--- backend/src/main/java/com/example/backend/controller/MemberController.java
+++ backend/src/main/java/com/example/backend/controller/MemberController.java
@@ -1,13 +1,17 @@
 package com.example.backend.controller;
 
 import com.example.backend.dto.Member.MemberDto;
+import com.example.backend.entity.maria.User;
 import com.example.backend.repository.MemberRepository;
 import com.example.backend.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,6 +28,13 @@ public class MemberController {
     public List<MemberDto> getMembers() {
 
         return memberService.getAllMembers();
+    }
+
+    @CrossOrigin
+    @GetMapping("/deptMember")
+    public ResponseEntity<List<MemberDto>> getDeptMembers(@AuthenticationPrincipal User user) {
+        List<MemberDto> deptMembers = memberService.getDeptMembers(user.getDept());
+        return ResponseEntity.ok(deptMembers);
     }
 
 }
