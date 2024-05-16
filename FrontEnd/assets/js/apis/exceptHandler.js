ffdafd37876b68efcfe2f2be661f237304e78391
seojw4973 <sgw0816@naver.com>
2024-05-16 09:27:27 +0900

    로그인 실패시 에러 메세지 변경

--- FrontEnd/assets/js/apis/exceptHandler.js
+++ FrontEnd/assets/js/apis/exceptHandler.js
@@ -1,11 +1,11 @@
 export const loginExceptionHandler = (res) => {
     if (res.status == 401) {
         // window.location.href = '../../../login/login.html';
-        alert('아이디 비밀번호 둘 중 하나는 틀린 듯?');
+        alert('로그인이 필요합니다.');
         window.location.href = 'http://localhost:3200/login';
     }
 
-    if (!res.ok) alert('로그인 에러 발생 뭔지는 몰라!');
+    if (!res.ok) alert('아이디 또는 비밀번호가 틀렸습니다. 다시 입력해주세요');
 
     return res;
 };
